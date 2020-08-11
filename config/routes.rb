237570a662b60716fa7dd6e52bc5b8d1de5d1ae8
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-08-11 16:01:32 +0200

    exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #et 'tasks', to: 'tasks#index'
+  #get "tasks/new", to: "tasks#new"
+  #get 'tasks/:id', to: 'tasks#show', as: :task
+
+  resources :tasks
 end
